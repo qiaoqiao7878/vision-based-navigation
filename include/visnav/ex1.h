5b0fbb6532f18d6538955f1fdbcb82636b5b891c
s0068 <ge82diw@mytum.de>
2020-05-01 13:28:07 +0200

    test ex0

--- include/visnav/ex1.h
+++ include/visnav/ex1.h
@@ -43,8 +43,20 @@ template <class T>
 Eigen::Matrix<T, 3, 3> user_implemented_expmap(
     const Eigen::Matrix<T, 3, 1>& xi) {
   // TODO SHEET 1: implement
+
+  Eigen::Matrix<T, 3, 3> w_hat;
+  w_hat << 0, -xi(2), xi(1), xi(2), 0, -xi(0), -xi(1), xi(0), 0;
+
+  double theta;
+  theta = sqrt(xi(0) * xi(0) + xi(1) * xi(1) + xi(2) * xi(2));
+  Eigen::Matrix<T, 3, 3> expmap;
+  expmap = Eigen::MatrixXd::Identity(3, 3);
+  if (theta > 1e-8) {
+    expmap = expmap + sin(theta) / theta * w_hat;
+    expmap = expmap + (1 - cos(theta)) / (theta * theta) * w_hat * w_hat;
+  }
   UNUSED(xi);
-  return Eigen::Matrix<T, 3, 3>();
+  return expmap;
 }
 
 // Implement log for SO(3)
@@ -52,8 +64,14 @@ template <class T>
 Eigen::Matrix<T, 3, 1> user_implemented_logmap(
     const Eigen::Matrix<T, 3, 3>& mat) {
   // TODO SHEET 1: implement
+  Eigen::Matrix<T, 3, 1> w = Eigen::MatrixXd::Zero(3, 1);
+  double theta = acos((mat(0, 0) + mat(1, 1) + mat(2, 2) - 1) / 2);
+  if (theta > 1e-8) {
+    w << mat(2, 1) - mat(1, 2), mat(0, 2) - mat(2, 0), mat(1, 0) - mat(0, 1);
+    w = theta * w / (2 * sin(theta));
+  }
   UNUSED(mat);
-  return Eigen::Matrix<T, 3, 1>();
+  return w;
 }
 
 // Implement exp for SE(3)
@@ -61,8 +79,34 @@ template <class T>
 Eigen::Matrix<T, 4, 4> user_implemented_expmap(
     const Eigen::Matrix<T, 6, 1>& xi) {
   // TODO SHEET 1: implement
+  Eigen::Matrix<T, 4, 4> expmap_SE3 = Eigen::MatrixXd::Identity(4, 4);
+
+  Eigen::Matrix<T, 3, 1> v;
+  v << xi(0), xi(1), xi(2);
+  Eigen::Matrix<T, 3, 1> w;
+  w << xi(3), xi(4), xi(5);
+
+  Eigen::Matrix<T, 3, 3> w_hat;
+  w_hat << 0, -w(2), w(1), w(2), 0, -w(0), -w(1), w(0), 0;
+
+  double theta = sqrt(w(0) * w(0) + w(1) * w(1) + w(2) * w(2));
+
+  if (theta > 1e-12) {
+    expmap_SE3.block(0, 0, 3, 3) += sin(theta) / theta * w_hat;
+    expmap_SE3.block(0, 0, 3, 3) +=
+        (1 - cos(theta)) / (theta * theta) * w_hat * w_hat;
+  }
+
+  Eigen::Matrix<T, 3, 3> J = Eigen::MatrixXd::Identity(3, 3);
+
+  if (theta > 1e-12) {
+    J += (1 - cos(theta)) / (theta * theta) * w_hat;
+    J += (theta - sin(theta)) / pow(theta, 3) * w_hat * w_hat;
+  }
+
+  expmap_SE3.block(0, 3, 3, 1) = J * v;
   UNUSED(xi);
-  return Eigen::Matrix<T, 4, 4>();
+  return expmap_SE3;
 }
 
 // Implement log for SE(3)
@@ -70,8 +114,38 @@ template <class T>
 Eigen::Matrix<T, 6, 1> user_implemented_logmap(
     const Eigen::Matrix<T, 4, 4>& mat) {
   // TODO SHEET 1: implement
+
+  Eigen::Matrix<T, 3, 1> t;
+  t << mat(0, 3), mat(1, 3), mat(2, 3);
+  Eigen::Matrix<T, 3, 1> w = Eigen::MatrixXd::Zero(3, 1);
+
+  double theta = acos((mat(0, 0) + mat(1, 1) + mat(2, 2) - 1) / 2);
+
+  if (theta > 1e-20) {
+    w << mat(2, 1) - mat(1, 2), mat(0, 2) - mat(2, 0), mat(1, 0) - mat(0, 1);
+    w = theta * w / (2 * sin(theta));
+  }
+
+  Eigen::Matrix<T, 3, 3> w_hat;
+  w_hat << 0, -w(2), w(1), w(2), 0, -w(0), -w(1), w(0), 0;
+
+  Eigen::Matrix<T, 3, 3> J_inv = Eigen::MatrixXd::Identity(3, 3);
+
+  J_inv = J_inv - 0.5 * w_hat;
+
+  if (theta > 1e-20) {
+    J_inv = J_inv + ((1 / (theta * theta)) -
+                     ((1 + cos(theta)) / (2 * theta * sin(theta)))) *
+                        w_hat * w_hat;
+  }
+
+  Eigen::Matrix<T, 3, 1> v;
+  v = J_inv * t;
+
+  Eigen::Matrix<T, 6, 1> xi;
+  xi << v(0), v(1), v(2), w(0), w(1), w(2);
   UNUSED(mat);
-  return Eigen::Matrix<T, 6, 1>();
+  return xi;
 }
 
 }  // namespace visnav
